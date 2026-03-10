03fabb702faacc6341d82304a68e8496e5b7b62f
Pranav Patil <pp4779185@gmail.com>
2026-03-10 23:16:41 +0530

    Added gitignore and removed node_modules

---
+++ controllers/userController.js
@@ -0,0 +1,40 @@
+const User = require("../models/User");
+
+// Create user
+exports.createUser = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Get all users
+exports.getUsers = async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+};
+
+// Get user by ID
+exports.getUserById = async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  res.json(user);
+};
+
+// Update user
+exports.updateUser = async (req, res) => {
+  const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(user);
+};
+
+// Delete user
+exports.deleteUser = async (req, res) => {
+  await User.findByIdAndDelete(req.params.id);
+  res.json({ message: "User deleted" });
+};
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+
+app.use(express.json());
+
+connectDB();
+
+// Import routes
+const userRoutes = require("./routes/userRoutes");
+
+// Use routes
+app.use("/api", userRoutes);
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
